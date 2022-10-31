b0d813b69d6c26d7bcf0ce9742ef434fbac84d3a
theFellandes <57584110+theFellandes@users.noreply.github.com>
2022-10-31 16:24:29 +0300

    Refactor -> settings.py
    Details:
    private file extention finder function updated to utilize rpartition function. Simplified the try-except blocks.
    
    get_path try-except blocks simplified.

--- conf/settings.py
+++ conf/settings.py
@@ -10,21 +10,14 @@ PATH_DICTIONARY = {
 }
 
 
-def __find_file_extension_from_source_code(source_code_name_with_extension: str) -> str:
+def __get_file_extension(source_code_name_with_extension: str) -> str:
     """
     Returns the file extension from source code
 
     :param source_code_name_with_extension: Source code name with extension
     :return: file extension
-    :exception IndexError: Entered source code name missing extension.
     """
-    splitted_source_code_name = source_code_name_with_extension.split('.')
-    try:
-        file_extension = splitted_source_code_name[1]
-
-    except IndexError:
-        raise IndexError('File Extension Missing.')
-
+    _, _, file_extension = source_code_name_with_extension.rpartition('.')
     return file_extension
 
 
@@ -36,15 +29,14 @@ def get_path(source_code_name_with_extension: str) -> str:
     :returns source_code_path: string version of the source code path
 
     :exception TypeError: Entered coding language format is wrong or does not exist.
-    :exception IndexError: Entered source code name missing extension.
     """
-    coding_language = __find_file_extension_from_source_code(source_code_name_with_extension)
+    coding_language = __get_file_extension(source_code_name_with_extension)
     coding_language_path = PATH_DICTIONARY.get(coding_language)
 
     try:
         source_code_path = os.path.join(coding_language_path, source_code_name_with_extension)
 
     except TypeError:
-        raise TypeError("Entered coding language does not exist.")
+        raise TypeError("Entered file does not exist in Source Codes folder")
 
     return source_code_path
