368bc0216b25a1c05cbde079c83d1d58ad1e6143
theFellandes <57584110+theFellandes@users.noreply.github.com>
2022-10-31 13:00:56 +0300

    * Reader helper class has the functionality for reading whole string.
    * Settings has the functionality for dynamic path finding
    
    * main.py cleaned

--- Utils/reader.py
+++ Utils/reader.py
@@ -1,18 +1,31 @@
-from collections import Iterable
+from dataclasses import dataclass
+from typing import Iterable
 
 
+@dataclass
 class Reader:
+    path: str
+    chunk_size: int = 1024
 
-    @staticmethod
-    def read_in_chunks(file_object, chunk_size: int = 1024) -> Iterable:
+    def read_in_chunks(self, file_object) -> Iterable:
         """ Yields data line by line """
         while True:
-            data = file_object.readline(chunk_size)
+            data = file_object.readline(self.chunk_size)
             if not data:
                 break
             yield data
 
-    def read_contents(self, path: str, chunk_size: int = 1024) -> Iterable:
+    def read_contents(self) -> Iterable:
         """ Yields lines """
-        with open(path, 'r', encoding='utf-8') as reader:
-            yield from self.read_in_chunks(reader, chunk_size)
+        with open(self.path, 'r', encoding='utf-8') as reader:
+            yield from self.read_in_chunks(reader)
+
+    def read_in_string(self) -> str:
+        """
+        Returns string version of the file contents
+
+        :returns file_content_as_string: string version of the file contents
+        """
+        with open(self.path, 'r', encoding='utf-8') as reader:
+            file_content_as_string = reader.read()
+        return file_content_as_string
--- conf/settings.py
+++ conf/settings.py
@@ -4,3 +4,47 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 SOURCE_CODE_DIR = os.path.join(BASE_DIR, 'SourceCodes')
 JAVA_PATH = os.path.join(SOURCE_CODE_DIR, 'Java')
 PYTHON_PATH = os.path.join(SOURCE_CODE_DIR, 'Python')
+PATH_DICTIONARY = {
+    'py': PYTHON_PATH,
+    'java': JAVA_PATH,
+}
+
+
+def __find_file_extension_from_source_code(source_code_name_with_extension: str) -> str:
+    """
+    Returns the file extension from source code
+
+    :param source_code_name_with_extension: Source code name with extension
+    :return: file extension
+    :exception IndexError: Entered source code name missing extension.
+    """
+    splitted_source_code_name = source_code_name_with_extension.split('.')
+    try:
+        file_extension = splitted_source_code_name[1]
+
+    except IndexError:
+        raise IndexError('File Extension Missing.')
+
+    return file_extension
+
+
+def get_path(source_code_name_with_extension: str) -> str:
+    """
+    Returns Java source code's path
+
+    :argument source_code_name_with_extension: string version of the source code file name
+    :returns source_code_path: string version of the source code path
+
+    :exception TypeError: Entered coding language format is wrong or does not exist.
+    :exception IndexError: Entered source code name missing extension.
+    """
+    coding_language = __find_file_extension_from_source_code(source_code_name_with_extension)
+    coding_language_path = PATH_DICTIONARY.get(coding_language)
+
+    try:
+        source_code_path = os.path.join(coding_language_path, source_code_name_with_extension)
+
+    except TypeError:
+        raise TypeError("Entered coding language does not exist.")
+
+    return source_code_path
