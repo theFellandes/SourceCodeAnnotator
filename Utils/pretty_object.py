7bf3f275d3cc86e1a3091bea7c59d45c702fa030
theFellandes <57584110+theFellandes@users.noreply.github.com>
2022-11-05 21:44:25 +0300

    Add pretty object wrapper
    A wrapper created for printing objects prettier to console for debug purposes

---
+++ Utils/pretty_object.py
@@ -0,0 +1,23 @@
+import astpretty
+import pprintpp
+from functools import wraps
+
+
+def pretty_object(func):
+    """ Pretty prints the returned object from the function """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        pprintpp.pprint(vars(func(*args, **kwargs)), indent=4)
+
+    return wrapper
+
+
+def python_ast_prettier(func):
+    """ Pretty prints Python AST returned from function """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        astpretty.pprint(func(*args, **kwargs))
+
+    return wrapper
