68bd38e9d0489ecf7409d34f54554d1d80e971d8
theFellandes <57584110+theFellandes@users.noreply.github.com>
2022-11-08 19:18:12 +0300

    Add test for as_dict function

--- Tests/test_source_code_controller.py
+++ Tests/test_source_code_controller.py
@@ -30,3 +30,6 @@ def test_java_source_code_print(java_source_code_parser):
 
 def test_python_source_code_print(python_source_code_parser):
     python_source_code_parser.generate_ast()
+
+def test_as_dict(java_source_code_parser):
+    assert type(java_source_code_parser.as_dict()) is type({})
