e223054b0f7af7095bfbe5d7cd0d8b8a0e385212
theFellandes <57584110+theFellandes@users.noreply.github.com>
2022-11-08 23:18:09 +0300

    Add comment finder regex functions.
    This functions has some issues that needs to be discussed. Also, some implementations needs to be discussed as well such as remove_escape_characters should be staticmethod or not?

--- Controller/source_code_controller.py
+++ Controller/source_code_controller.py
@@ -1,5 +1,6 @@
 import ast
 import dataclasses
+import re
 
 import javalang
 from dataclasses import dataclass, field
@@ -15,6 +16,8 @@ from Utils.reader import Reader
 class SourceCodeParserController:
     source_code_file_name: str
     source_code_string: str = field(init=False, default="")
+    single_line_comments_list: list = field(init=False, default_factory=list)
+    multi_line_comments_list: list = field(init=False, default_factory=list)
 
     def __post_init__(self) -> None:
         """
@@ -41,23 +44,62 @@ class SourceCodeParserController:
         operation = {"java": self.__generate_java_ast, "py": self.__generate_python_ast}
         operation.get(self.source_code_extension)()
 
-    def remove_escape_characters(self) -> None:
+    @staticmethod
+    def remove_escape_characters(raw_string: str) -> str:
         """
         Removes first 32 characters of the ASCII table from source code string
+
+        raw_string: Input string that has escape characters
 
         escapes: The first 32 characters of ASCII table
         translator: A translation table for the escape characters
         """
+        # TODO: Discuss this function as to make it static or not.
         escapes = "".join([chr(char) for char in range(1, 32)])
         translator = str.maketrans("", "", escapes)
-        self.source_code_string = self.source_code_string.translate(translator)
+        return raw_string.translate(translator)
+
+    def capture_multi_line_comments(self) -> None:
+        """
+        Finds multi-line comments
+
+        Dark Compiler magic java:
+        \/\* +\*(\s+([a-zA-Z]+\s+)+)\*\/
+        /* <ANYTHING> */
+
+        Darker Compiler magic python:
+        Apparently Python doesn't require this regex to find multi-line comments
+        Since Python AST covers multi-line comments
+        \"\"\"[^"]*\"\"\"
+        \'\'\'[^']*\'\'\'
+        """
+        # TODO: Discussion, This function can be static
+        self.multi_line_comments_list = re.findall('\/\* +\*(\s+([a-zA-Z]+\s+)+)\*\/', self.source_code_string)
+
+
+    def capture_single_line_comments(self) -> None:
+        """
+        Finds single-line comments
+
+        Java:
+        \/\/[a-zA-Z]+ [a-zA-Z]+
+
+        Python:
+        \#[a-zA-Z]+ [a-zA-Z]+
+        """
+        # TODO: Discussion, This function can be static
+        if self.source_code_extension == 'java':
+            self.single_line_comments_list = re.findall('^(\/\/)[a-zA-Z]+ [a-zA-Z]+', self.source_code_string)
+
+        else:
+            self.single_line_comments_list = re.findall('(^#)[a-zA-Z]+ [a-zA-Z]+', self.source_code_string)
 
     @get_time
     @pretty_object
     def __generate_java_ast(self) -> CompilationUnit:
         """Prints java_ast to the console"""
         # Clear the escape characters for Java.
-        self.remove_escape_characters()
+        self.source_code_string = self.remove_escape_characters(self.source_code_string)
 
         tree: CompilationUnit = javalang.parse.parse(self.source_code_string)
         return tree.types[0].body[1]
@@ -66,5 +108,8 @@ class SourceCodeParserController:
     @python_ast_prettier
     def __generate_python_ast(self) -> ast:
         """Prints python_ast to the console"""
+        # Since Python uses tabs and newlines, only comments extracted
+        # requires the escape character deletion.
+
         python_ast = ast.parse(self.source_code_string)
         return python_ast.body[5]
