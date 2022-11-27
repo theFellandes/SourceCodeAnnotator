994db05d6bf6ea6214cf4943145abb854eba0b0b
theFellandes <57584110+theFellandes@users.noreply.github.com>
2022-11-27 15:28:58 +0300

    Add write_ast_to_file to python_ast.py

--- Models/SourceCode/python_ast.py
+++ Models/SourceCode/python_ast.py
@@ -4,16 +4,24 @@ from dataclasses import dataclass
 from Utils.ast_to_file import ast_to_file
 from Utils.pretty_object import python_ast_prettier
 from Utils.time_util import get_time
-from ast_base import ASTBase
+from Models.SourceCode.ast_base import ASTBase
 
 
 @dataclass
 class PythonAST(ASTBase):
 
+    def generate_ast(self) -> ast:
+        """Prints python_ast to the console"""
+        # Since Python uses tabs and newlines, only comments extracted
+        # requires the escape character deletion.
+
+        python_ast = ast.parse(self.source_code_string)
+        return python_ast
+
     @ast_to_file
     @python_ast_prettier
     @get_time
-    def generate_ast(self) -> ast:
+    def write_ast_to_file(self) -> ast:
         """Prints python_ast to the console"""
         # Since Python uses tabs and newlines, only comments extracted
         # requires the escape character deletion.
