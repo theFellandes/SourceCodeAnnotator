f69500e76e17aba0ee2ffba86389d845a90231b4
theFellandes <57584110+theFellandes@users.noreply.github.com>
2022-11-27 15:29:09 +0300

    Add write_ast_to_file to java_ast.py

--- Models/SourceCode/java_ast.py
+++ Models/SourceCode/java_ast.py
@@ -6,16 +6,21 @@ from javalang.tree import CompilationUnit
 from Utils.ast_to_file import ast_to_file
 from Utils.pretty_object import pretty_object
 from Utils.time_util import get_time
-from ast_base import ASTBase
+from Models.SourceCode.ast_base import ASTBase
 
 
 @dataclass
 class JavaAST(ASTBase):
 
+    def generate_ast(self) -> CompilationUnit:
+        """Prints java_ast to the console"""
+        tree: CompilationUnit = javalang.parse.parse(self.source_code_string)
+        return tree
+
     @ast_to_file
     @pretty_object
     @get_time
-    def generate_ast(self) -> CompilationUnit:
+    def write_ast_to_file(self):
         """Prints java_ast to the console"""
         tree: CompilationUnit = javalang.parse.parse(self.source_code_string)
         return tree
