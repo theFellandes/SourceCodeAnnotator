fcf9958e3c968fe0e1f168e6e2e493ffb03433ac
theFellandes <57584110+theFellandes@users.noreply.github.com>
2022-11-30 20:07:33 +0300

    Refactor ast_base.py

--- Models/SourceCode/ast_base.py
+++ Models/SourceCode/ast_base.py
@@ -1,31 +1,10 @@
 import dataclasses
-from dataclasses import dataclass, field
-
-from Utils.reader import Reader
-from Utils.writer import Writer
-from conf import settings
+from dataclasses import dataclass
 
 
 @dataclass
 class ASTBase:
-    source_code_file_name: str
-    source_code_string: str = field(init=False, default="")
-    single_line_comments_list: list = field(init=False, default_factory=list)
-    multi_line_comments_list: list = field(init=False, default_factory=list)
-
-    def __post_init__(self) -> None:
-        """
-        Initializes the Source Code Parser rest of the attributes
-
-        Initializes:
-        source_code_path: string path of the source code
-        reader: the Reader object for the source_code_path
-        source_code_string: source code contents saved to string
-        """
-        self.source_code_path = settings.get_path(self.source_code_file_name)
-        self.reader = Reader(self.source_code_path)
-        self.writer = Writer(settings.REPORT_PATH)
-        self.source_code_string = self.reader.read_in_string()
+    source_code_string: str
 
     def as_dict(self) -> dict:
         """Returns dict of the class"""
