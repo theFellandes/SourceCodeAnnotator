d8c3b823e947550360f3f4a4507815cbd3e138a5
theFellandes <57584110+theFellandes@users.noreply.github.com>
2022-11-30 20:07:46 +0300

    Refactor java_controller.py

--- Controller/java_controller.py
+++ Controller/java_controller.py
@@ -1,11 +1,47 @@
-from dataclasses import dataclass
+import re
+from dataclasses import dataclass, field
 
 from Controller.base_controller import BaseController
+from Controller.report_controller import ReportController
 from Models.SourceCode.java_ast import JavaAST
 
 
 @dataclass
 class JavaController(BaseController):
+    def __post_init__(self):
+        super().__post_init__()
+        self.java_ast = JavaAST(self.source_code_string)
 
-    def generate_comment(self, java_ast: JavaAST):
+    def get_single_line_comments(self) -> list:
+        """ Returns single line comments """
+        single_line_comments_list = re.findall(
+            "(?://[^\n]*|/\*(?:(?!\*/).)*\*/)", self.java_ast.source_code_string
+        )
+        return single_line_comments_list
+
+    def get_multi_line_comments(self) -> list:
+        """ Returns multi line comments """
+        multi_line_comments_list = (
+            re.findall(
+                "(/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/)|(//.*)",
+                self.java_ast.source_code_string,
+            )
+        )
+        return multi_line_comments_list
+
+    def get_doc_comments(self) -> list:
+        """ Returns doc comments """
+        doc_comments_list = (
+            re.findall(
+                "(/\*\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/)|(//.*)",
+                self.java_ast.source_code_string,
+            )
+        )
+        return doc_comments_list
+
+    def generate_report(self):
+        report_controller = ReportController(self.writer, self)
+        report_controller.generate_report()
+
+    def generate_comment(self):
         pass
