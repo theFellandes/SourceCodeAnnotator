fa2db41835645f81aedcdc7c61ca70f2881aca2f
Oğuzhan Güngör <57584110+theFellandes@users.noreply.github.com>
2022-12-02 12:01:56 +0300

    Remove legacy Entities
    These entities hasn't used for a while.

--- Models/Entities/compilation_unit.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass, field
-from class_declaration import ClassDeclaration
-
-
-@dataclass
-class CompilationUnit:
-    package: None
-    imports: list[str] = field(default_factory=list)
-    types: list[ClassDeclaration] = field(default_factory=list)
--- Models/Entities/java_ast.py
+++
@@ -1,6 +0,0 @@
-from dataclasses import dataclass, field
-
-
-@dataclass
-class JavaAST:
-    imports: list[str] = field(default_factory=list)
