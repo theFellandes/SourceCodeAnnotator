c66dc11e2d9c5da8af2df62eb833e42016d15f3d
theFellandes <57584110+theFellandes@users.noreply.github.com>
2022-12-02 14:41:24 +0300

    Add SimpleLoop for java

---
+++ SourceCodes/Java/SimpleLoop.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello"); //asdf
+        simpleStatement("World");
+    }
+
+    public static void simpleStatement(String inputString){
+        for(int i = 0; i < 10; i++){
+            System.out.println("World");
+        }
+    }
+}
