6cb46f9cc1edc7f7cb5e600204e5086fe6715845
theFellandes <57584110+theFellandes@users.noreply.github.com>
2022-12-09 12:47:53 +0300

    Add permutations to the generated comments
    Changed the get_generated_comment to get_generated_comments_list

--- Utils/NLP/Stanza/stanza_module.py
+++ Utils/NLP/Stanza/stanza_module.py
@@ -1,3 +1,5 @@
+import itertools
+
 import stanza
 from stanza.pipeline.core import DownloadMethod
 from re import finditer
@@ -15,17 +17,22 @@ class NameAnalyzer:
                                    download_method=DownloadMethod.REUSE_RESOURCES,
                                    model_dir='.\\Stanza')
 
-    def get_generated_comment(self, name: str) -> str:
-        splunk_name = self.split_function_name(name)
-        doc = self.nlp(splunk_name)
-        return self.generate_comment(doc.sentences)
+    def get_generated_comments_list(self, class_name: str, function_name: str) -> list[str]:
+        splunk_name_list = self.split_function_name(function_name)
+        permutations_of_name_list = self.get_permutations_list(splunk_name_list)
 
-    def split_function_name(self, function_name: str) -> str:
+        list_of_comments = []
+        for permutation in permutations_of_name_list:
+            doc = self.nlp(permutation)
+            list_of_comments.append(self.generate_comment(class_name, doc.sentences))
+        return list_of_comments
+
+    def split_function_name(self, function_name: str) -> list[str]:
         if '_' in function_name:
             function_word_list = function_name.split('_')
         else:
             function_word_list = self.camel_case_split(function_name)
-        return ' '.join(function_word_list)
+        return function_word_list
 
     @staticmethod
     def camel_case_split(identifier) -> list[str]:
@@ -33,15 +40,23 @@ class NameAnalyzer:
         return [m.group(0) for m in matches]
 
     @staticmethod
-    def generate_comment(sentences: list) -> str:
-        string_builder = ''
+    def get_permutations_list(list_of_words: list[str]) -> list[str]:
+        return [' '.join(permutation) for permutation in itertools.permutations(list_of_words, len(list_of_words))]
+
+    @staticmethod
+    def generate_comment(class_name: str, sentences: list) -> str:
+        coherent_sentence = {
+            'CLASS': class_name,
+            'NOUN': ''
+        }
         for sentence in sentences:
             for word in sentence.words:
                 if word.pos == 'VERB':
-                    string_builder += f'{word.text.capitalize()}s '
+                    coherent_sentence[word.pos] = f'{word.text.capitalize()}'
                 else:
-                    string_builder += f'{word.text.capitalize()} '
-        return string_builder
+                    coherent_sentence[word.pos] = coherent_sentence.get(word.pos) + f'{word.text.capitalize()} '
+        return f"{coherent_sentence.get('VERB')}s {coherent_sentence.get('CLASS')} class' " \
+               f"{coherent_sentence.get('NOUN').rstrip()}"
 
 
 if __name__ == '__main__':
@@ -50,13 +65,13 @@ if __name__ == '__main__':
     # print(na.parse_function_name('getAnotherUserId'))
     # print(na.parse_function_name('nameAnalyser'))
     # print(na.get_generated_comment('getUserId'))
-    print(na.get_generated_comment('setUserId'))
-    print(na.get_generated_comment('userIdSetter'))
+    print(na.get_generated_comments_list('Employee', 'getUserId')) # Returns userId of Employee Class
+    # print(na.get_generated_comment('userIdSetter'))
 
     # Beyni yanan kısımlar
     # print(na.get_generated_comment('userIdSet'))
     # print(na.get_generated_comment('generateUserId'))
-    print(na.get_generated_comment('increment_number_by_one'))
+    # print(na.get_generated_comment('increment_number_by_one'))
 
     # Boss fight #
     # print(na.get_generated_comment('nameAnalyser')) # Analyses the name?
