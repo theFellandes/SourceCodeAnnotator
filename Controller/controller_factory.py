199db94c80657b4026d323e188f7b7594611566a
theFellandes <57584110+theFellandes@users.noreply.github.com>
2022-12-12 11:12:41 +0300

    Add controller_factory to get_controller

---
+++ Controller/controller_factory.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+
+from Controller.java_controller import JavaController
+from Controller.python_controller import PythonController
+from conf import settings
+
+
+@dataclass
+class ControllerFactory:
+
+    source_code_file_name: str
+
+    def get_controller(self):
+        match settings.get_file_extension(self.source_code_file_name):
+            case 'java':
+                return JavaController(self.source_code_file_name)
+            case 'py':
+                return PythonController(self.source_code_file_name)
+            case _:
+                return NotImplementedError
