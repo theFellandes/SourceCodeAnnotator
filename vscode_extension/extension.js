a5f711648a33a4f0357b375a710d0e0954b6e794
Aydin Burak 'Kakule' Kuscu <a.brkkuscu@gmail.com>
2023-03-10 20:58:44 +0300

    Add vscode extension sub-project

---
+++ vscode_extension/extension.js
@@ -0,0 +1,71 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+const path = require('path');
+const { spawn } = require('child_process');
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	// console.log('Congratulations, your extension "lazydoc" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('lazydoc.commentCurrentFileChatGPT', function () {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from ChatGPT!');
+	});
+
+	context.subscriptions.push(disposable);
+
+	disposable = vscode.commands.registerCommand('lazydoc.commentCurrentFile', async function () {
+		// Get the active text editor
+		const activeEditor = vscode.window.activeTextEditor;
+		if (!activeEditor) {
+			vscode.window.showErrorMessage('No active editor found!');
+			return;
+		}
+
+		// Get the code and send it to Annotator
+		// const text = activeEditor.document.getText();
+
+		// Get the URI (Uniform Resource Identifier) for the file that is open in the editor
+		const fileUri = activeEditor.document.uri;
+		// Convert the URI to a file path
+		const filePath = fileUri.fsPath;
+
+		const pythonProcess = await spawn('python', ['-u', path.join(__dirname, '../extension.py'), 'lazydoc', filePath]);
+
+		// This listener 
+		pythonProcess.stdout.on('data', (data) => {
+			const wholeDocumentRange = activeEditor.document.validateRange(new vscode.Range(0, 0, Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER));
+			activeEditor.edit((editBuilder) => {
+				vscode.window.showInformationMessage(`${data}`);
+				editBuilder.replace(wholeDocumentRange, `${data}`);
+			});
+		});
+
+		pythonProcess.on('close', (code) => {
+			console.log(`child process exited with code ${code}`);
+		});
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+function deactivate() { }
+
+module.exports = {
+	activate,
+	deactivate
+}
