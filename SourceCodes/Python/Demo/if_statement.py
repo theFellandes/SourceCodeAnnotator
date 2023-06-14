ab033fba9c5a7da8113bc0d344648516f43d410e
theFellandes <57584110+theFellandes@users.noreply.github.com>
2023-06-14 19:37:26 +0300

    If {condition}; {body}

--- SourceCodes/Python/Demo/if_statement.py
+++ SourceCodes/Python/Demo/if_statement.py
@@ -1,11 +1,29 @@
-def if_statement():
-    i = 0
-    if i < 10:
+import random
+from dataclasses import dataclass
+from typing import Any, Union
+
+def if_statement(number):
+    x = 1 // 2
+    for i in number:
         print(i)
-    elif i <= 10:
+
+    for i in range(0, 10):
+        print(x)
+
+    if number > 12:
+        number = write()
+
+    if condition_function('dgsfgfsdgfsdgsfdg', 'sdfgdsfgsdfgsdfg', 'sdfgdsfgsdfgsdfg'):
+        print('True')
+
+    for i in range(0, len(number)):
         print(i)
-    else:
-        print(i)
+
+    if x >= 5:
+        print(x)
+        return 5
+    return x
+
 
 
 if __name__ == '__main__':
