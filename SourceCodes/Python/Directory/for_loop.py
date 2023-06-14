57cb9e0fbf6f8efcbd1611cda6aaf802fd8c1802
theFellandes <57584110+theFellandes@users.noreply.github.com>
2023-06-14 19:25:13 +0300

    Directory commenting

---
+++ SourceCodes/Python/Directory/for_loop.py
@@ -0,0 +1,4 @@
+def for_loop():
+    temp_list = []
+    for i in range(1, 10):
+        temp_list.append(i)
---
+++ SourceCodes/Python/Directory/if_statement.py
@@ -0,0 +1,30 @@
+import random
+from dataclasses import dataclass
+from typing import Any, Union
+
+def if_statement(number):
+    x = 1 // 2
+    for i in number:
+        print(i)
+
+    for i in range(0, 10):
+        print(x)
+
+    if number > 12:
+        number = write()
+
+    if condition_function('dgsfgfsdgfsdgsfdg', 'sdfgdsfgsdfgsdfg', 'sdfgdsfgsdfgsdfg'):
+        print('True')
+
+    for i in range(0, len(number)):
+        print(i)
+
+    if x >= 5:
+        print(x)
+        return 5
+    return x
+
+
+
+if __name__ == '__main__':
+    if_statement()
